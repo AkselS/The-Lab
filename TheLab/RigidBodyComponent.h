8ec2671f5d78ddae5b9f492aae4d42452037cd4e
Andrew Freebrey <andrewfreebrey@live.co.uk>
2016-04-10 23:04:31 +0100

    Physics Update 1
    Rigid body update method created
    All get set functions made

--- TheLab/RigidBodyComponent.h
+++ TheLab/RigidBodyComponent.h
@@ -6,13 +6,40 @@ class RigidBodyComponent
 {
 public:
 	RigidBodyComponent();
+	RigidBodyComponent(float _mass);
 	~RigidBodyComponent();
 
 	void Update(float dt);
+	D3DXVECTOR3 deltaPosition(float dt);
+
+#pragma region Get/Set Functions
+
+	D3DXVECTOR3 getVelocity();
+	void setVelocity(D3DXVECTOR3 _velocity);
+
+	D3DXVECTOR3 getAcceleration();
+	void setAcceleration(D3DXVECTOR3 _acceleration);
+
+	D3DXVECTOR3 getForce();
+	void setForce(D3DXVECTOR3 _force);
+
+	float getMass();
+	void setMass(float _mass);
+
+	bool getUseGravity();
+	void setUseGravity(bool _useGravity);
+
+#pragma endregion
 
 protected:
-	D3DXVECTOR3 Velocity;
-	D3DXVECTOR3 Acceleration;
+	D3DXVECTOR3 velocity;
+	D3DXVECTOR3 acceleration;
+	D3DXVECTOR3 force;
 
+	float mass;
+
+	bool useGravity;
+
+	const float g = 9.81f;
 };
 
